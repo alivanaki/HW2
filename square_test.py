e52d620e4ab1f0760032a8df49237d54e0ffb45e
test <test@gmail.com>
2023-07-22 01:32:31 +0430

    Add unit test for get side function of square.

--- square_test.py
+++ square_test.py
@@ -8,6 +8,9 @@ class MyTestCase(unittest.TestCase):
         square = Square(5)
         self.assertEqual(square.computeArea(), 25)
 
+    def test_get_side(self):
+        square = Square(5)
+        self.assertEqual(square.getSide(), 5)
 
 if __name__ == '__main__':
     unittest.main()
