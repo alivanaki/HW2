c89ebb4e87bc0568e9f726443b68e48349c9f8b6
test <test@gmail.com>
2023-07-21 20:48:02 +0430

    Add unit test for get height and width function at Rectangle class.

--- rectangle_test.py
+++ rectangle_test.py
@@ -7,6 +7,14 @@ class TestRectangle(unittest.TestCase):
         rect = Rectangle(5, 10)
         self.assertEqual(rect.computeArea(), 50)
 
+    def test_get_width(self):
+        rect = Rectangle(width=5, height=10)
+        self.assertEqual(rect.getWidth(), 5)
+
+    def test_get_height(self):
+        rect = Rectangle(width=5, height=10)
+        self.assertEqual(rect.getHeight(), 10)
+
 
 if __name__ == '__main__':
     unittest.main()
